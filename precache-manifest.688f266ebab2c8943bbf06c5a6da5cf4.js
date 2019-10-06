1923b122b9729ed9895aea8d4050b6558623a59f
Thomas <thomasskinner76@gmail.com>
2019-10-06 18:57:52 -0400

    first commit

---
+++ precache-manifest.688f266ebab2c8943bbf06c5a6da5cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00e05236ae3315f5edc4",
+    "url": "/static/js/main.00e05236.chunk.js"
+  },
+  {
+    "revision": "c7b2291e239ac4444219",
+    "url": "/static/js/1.c7b2291e.chunk.js"
+  },
+  {
+    "revision": "00e05236ae3315f5edc4",
+    "url": "/static/css/main.2d4dfd2c.chunk.css"
+  },
+  {
+    "revision": "2ceca3e3603bf5b4ee3c030b04bb3eca",
+    "url": "/index.html"
+  }
+];
